6f1a49e67fdb94675ffb4f5ccdbeaf914db2f3ec
CARLOSIJM <carlosidjmdz@gmail.com>
2023-07-30 00:33:14 -0600

    variables

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+   public static void main(String[] args) {
+    //Declaracion de una variable.
+    int speed;
+    //Asignamos un valor a una variable
+    speed = 10;
+    System.out.println(speed);
+
+    //Inicialización de una variables con un valor inicial
+    int salary = 1000;
+    String eployeeName = "Carlos Ivan";
+    System.out.println(eployeeName);
+    }
+}
