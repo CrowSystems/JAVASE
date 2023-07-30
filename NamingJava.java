594211ad2b4668543e24747394348831603076ca
CARLOSIJM <carlosidjmdz@gmail.com>
2023-07-29 21:46:51 -0600

    clase HolaMundo

--- NamingJava.java
+++ NamingJava.java
@@ -1,5 +1,11 @@
 public class NmingJava {
     public static void main(String args[]){
-        
+        int celphone = 33337777;
+        int celPhone = 55553333;
+
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $contryName = "Spain";
     }
 }
