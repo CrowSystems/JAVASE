9c267499b913c6656136c18f90c0182a25c644aa
CarlosIDJM <carlosidjmdz@gmail.com>
2023-08-28 22:15:42 -0600

    funciones con java

---
+++ FUNCIONES/MathematicOperations.java
@@ -0,0 +1,17 @@
+package FUNCIONES;
+
+public class MathematicOperations {
+
+    public static void main(String[] args) {
+
+        //declaración de variables.
+        double x = 2.1;
+        double y = 3;
+
+        //Devuelve un entero haca arriba
+        System.out.println(Math.ceil(x));
+
+        
+    }
+    
+}
