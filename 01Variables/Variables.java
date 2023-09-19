9a328f9637b35c75dbe42d9df02bcc5277abbb41
CarlosIDJM <carlosidjmdz@gmail.com>
2023-09-18 23:07:58 -0600

    Variables con Java

---
+++ 01Variables/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declarar la variable speed de tipo int (números enteros)
+        int speed;
+
+        //Actualizar el contenido de la variable speed
+        speed = 10;
+
+        //Declara una variable y asignarle un valor al mismo tiempo.
+        int salary = 1000;
+        
+
+        //crear una variable de tipo String:
+        String employeeName = "Anahí Salgado";
+    }
+}
