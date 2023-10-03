ebcae0b22c04d0753858007aea472658de649473
CarlosIDJM <carlosidjmdz@gmail.com>
2023-10-03 07:26:39 -0600

    Ciclos While

--- CICLOS/WhileLoop.java
+++ CICLOS/WhileLoop.java
@@ -1,3 +1,16 @@
 public class WhileLoop {
+
+    static booblean isTurnOnLight = false;
     
+    public static void main(String[] args) {
+        
+    }
+
+    public static void printSOS(){
+        System.out.println(". . . _ _ _ . . .");
+    }
+
+    public static boolean turnOnOffLight(){
+        
+    }
 }
